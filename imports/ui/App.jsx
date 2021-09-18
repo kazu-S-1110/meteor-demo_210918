73d8935efb23fa042f437def1ff8a878884ba6f8
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-09-19 00:10:26 +0900

    mongodbをコード上で書いて、Reactからfetch。dataが更新されたらre-renderされるらしい。

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,22 +1,30 @@
 import React from 'react'
+import { TasksCollection } from '../api/TasksCollection.js'
+import { useTracker } from 'meteor/react-meteor-data'
+
 import { Hello } from './Hello.jsx'
 import { Info } from './Info.jsx'
 import { Task } from './Task'
 
-const tasks = [
-  { _id: 1, text: "First Task" },
-  { _id: 2, text: "Second Task" },
-  { _id: 3, text: "Third Task" },
-]
+// const tasks = [
+//   { _id: 1, text: "First Task" },
+//   { _id: 2, text: "Second Task" },
+//   { _id: 3, text: "Third Task" },
+// ]
 
-export const App = () => (
+export const App = () => {
 
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello />
-    <Info />
-    <ul>
-      {tasks.map(task => <Task key={task._id} task={task} />)}
-    </ul>
-  </div>
-)
+
+  const tasks = useTracker(() => TasksCollection.find({}).fetch())
+  return (
+
+    <div>
+      <h1>Welcome to Meteor!</h1>
+      <Hello />
+      <Info />
+      <ul>
+        {tasks.map(task => <Task key={task._id} task={task} />)}
+      </ul>
+    </div>
+  )
+}
