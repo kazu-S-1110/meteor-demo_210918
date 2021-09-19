ac816218f6ffd08fc626c3e3cbdb06a599fae4db
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-09-19 20:03:15 +0900

    taskFormを作成。追加したものが一番上にくるように変更。

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -5,6 +5,7 @@ import { useTracker } from 'meteor/react-meteor-data'
 import { Hello } from './Hello.jsx'
 import { Info } from './Info.jsx'
 import { Task } from './Task'
+import { TaskForm } from './TaskForm.jsx'
 
 // const tasks = [
 //   { _id: 1, text: "First Task" },
@@ -15,13 +16,14 @@ import { Task } from './Task'
 export const App = () => {
 
 
-  const tasks = useTracker(() => TasksCollection.find({}).fetch())
+  const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch())
   return (
 
     <div>
       <h1>Welcome to Meteor!</h1>
       <Hello />
       <Info />
+      <TaskForm />
       <ul>
         {tasks.map(task => <Task key={task._id} task={task} />)}
       </ul>
---
+++ imports/ui/TaskForm.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import { TasksCollection } from '../api/TasksCollection'
+
+export const TaskForm = () => {
+  const [text, setText] = useState("")
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!text) return
+
+    TasksCollection.insert({
+      text: text.trim(),
+      createdAt: new Date()
+    })
+
+    setText("")
+  }
+  return (
+    <form className="task-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Type to add new tasks"
+      />
+      <button type="submit">Add Task</button>
+    </form>
+  )
+}
